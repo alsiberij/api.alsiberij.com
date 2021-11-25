73dd17149dcd08cfe26be59a3737599c74f30fc7
Alexander Sibirtsev <alsiberij@gmail.com>
2021-11-25 21:40:25 +0300

    Creator for user entity added

---
+++ entities/creators/UserCreator.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class UserCreator extends EntityCreator {
+
+    public function newInstance(int $ID): ?User {
+        $result = $this->db->prepare("SELECT * FROM {$this->tableName()} WHERE id = :ID");
+        $result->bindParam(':ID', $ID, PDO::PARAM_INT);
+        if (!$result->execute()) {
+            http_response_code(500);
+            echo(json_encode(['error'=>'Internal DB error']));
+            die;
+        }
+        if ($r = $result->fetch(PDO::FETCH_ASSOC)) {
+            return new User($r['ID'], $r['activation'], $r['accessToken'], $r['administrator'], $r['moderator'],
+                $r['privacy'], $r['nickname'], $r['email'], $r['emailPrivacy'], $r['password'], $r['registrationDate'],
+                $r['balance'], $r['balancePrivacy'], $r['avatar'], $r['birthday'], $r['location'], $r['bio'],
+                $r['likes'], $r['comments'], $r['paidOrders'], $r['lastSeenTime'], $r['lastSeenTimePrivacy']);
+        } else {
+            return null;
+        }
+    }
+
+    protected function tableName(): string {
+        return 'users';
+    }
+
+}
