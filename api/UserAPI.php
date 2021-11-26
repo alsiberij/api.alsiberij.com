5c6b7e64e3e2ecd5861b18bffdd8a7a7948e231e
Alexander Sibirtsev <alsiberij@gmail.com>
2021-11-26 11:45:35 +0300

    Get method for User API implemented

--- api/UserAPI.php
+++ api/UserAPI.php
@@ -24,7 +24,57 @@ class UserAPI extends API implements Retrievable {
     }
 
     public function get(): void {
+        $rawUserIDs = $_POST['userIDs'] ?? $_GET['userIDs'] ?? null;
+        $userIDs = json_decode($rawUserIDs);
+        if (!$userIDs) {
+            http_response_code(400);
+            echo(json_encode(['error' => 'Invalid user IDs. Use JSON notation']));
+            die;
+        }
+        array_walk($userIDs, function (&$value, $key) {
+            if (!is_int($value)) {
+                http_response_code(400);
+                echo(json_encode(['error' => "Invalid user ID ($value)"]));
+                die;
+            }
+        });
 
+        $usersList = [];
+        foreach ($userIDs as $userID) {
+            $user = $this->creator->newInstance($userID);
+            if ($user) {
+                $accessAllData = false;
+                if ($this->authorizedUser &&
+                    ($this->authorizedUser->getID() == $user->getID() || $this->authorizedUser->isAdministrator())) {
+                    $accessAllData = true;
+                }
+
+                $user = $user->toArray();
+
+                if ($accessAllData || $user['privacy']) {
+                    unset($user['password']);
+                    unset($user['salt']);
+                    unset($user['accessToken']);
+                    unset($user['isActivated']);
+                    if (!$accessAllData) {
+                        if (!$user['emailPrivacy']) {
+                            $user['email'] = null;
+                        }
+                        if (!$user['balancePrivacy']) {
+                            $user['balance'] = null;
+                        }
+                        if (!$user['lastSeenTimePrivacy']) {
+                            $user['lastSeenTime'] = null;
+                        }
+                    }
+                    $usersList[] = $user;
+                }
+            }
+        }
+
+        http_response_code(200);
+        echo(json_encode(['response'=>$usersList]));
+        die;
     }
 
     public function getAll(): void {
