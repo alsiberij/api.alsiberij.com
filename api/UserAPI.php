d868ae654d64b00a0b91dafcdbfb08db3fa7614f
Alexander Sibirtsev <alsiberij@gmail.com>
2021-11-27 15:00:38 +0300

    AOI method create implemented

--- api/UserAPI.php
+++ api/UserAPI.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class UserAPI extends API implements Retrievable {
+class UserAPI extends API implements Retrievable, Creatable {
 
     public function __construct() {
         parent::__construct();
@@ -16,6 +16,10 @@ class UserAPI extends API implements Retrievable {
             }
             case 'getAll': {
                 $this->getAll();
+                return;
+            }
+            case 'create': {
+                $this->create();
                 return;
             }
 
@@ -119,4 +123,56 @@ class UserAPI extends API implements Retrievable {
         echo(json_encode(['response'=>$usersList]));
     }
 
+    public function create(): void {
+        $nickname = $_POST['nickname'] ?? $_GET['nickname'] ?? '';
+        $email = $_POST['email'] ?? $_GET['email'] ?? '';
+        $password = $_POST['password'] ?? $_GET['password'] ?? '';
+
+        $nicknameErrors = User::validateNickname($nickname);
+        $emailErrors = User::validateEmail($email);
+        $passwordErrors = User::validatePassword($password);
+
+        $errors = array_merge($nicknameErrors, $emailErrors, $passwordErrors);
+        if (!empty($errors)) {
+            http_response_code(400);
+            echo(json_encode(['errors'=>$errors]));
+            die;
+        }
+
+        $salt = User::generateSalt();
+        $saltMD5 = md5($salt);
+        $password = md5(substr($saltMD5, 0, 16) . $password . substr($saltMD5, 16, 16));
+
+        //TODO Unite in transaction
+
+        $query = 'INSERT INTO users (nickname, email, password, salt) VALUES (:nickname, :email, :password, :salt);';
+        $result = $this->db->prepare($query);
+        $result->bindParam(':nickname', $nickname);
+        $result->bindParam(':email', $email);
+        $result->bindParam(':password', $password);
+        $result->bindParam(':salt', $salt);
+        if (!$result->execute()) {
+            http_response_code(400);
+            echo(json_encode(['error' => 'Query can not be executed']));
+            die;
+        }
+
+        $query = 'INSERT INTO activations (userID, token) SELECT ID, :token FROM users WHERE email = :email;';
+        $result = $this->db->prepare($query);
+        $result->bindParam(':email', $email);
+        $activationToken = Activation::generateToken();
+        $result->bindParam(':token', $activationToken);
+        if (!$result->execute()) {
+            http_response_code(400);
+            echo(json_encode(['error' => 'Query can not be executed']));
+            die;
+        }
+
+        http_response_code(200);
+        echo(json_encode(['response' => 'Sign Up successful']));
+    }
+
+    public function delete(): void {
+
+    }
 }
