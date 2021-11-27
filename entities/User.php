1703947176c665089db2d4d1dd48a4bdd3178379
Alexander Sibirtsev <alsiberij@gmail.com>
2021-11-27 14:57:53 +0300

    Class constants for data constraints added. Corresponded validators added

--- entities/User.php
+++ entities/User.php
@@ -25,6 +25,89 @@ class User extends Entity {
     protected DateTime $lastSeenTime;
     protected bool $lastSeenTimePrivacy;
 
+    public const NICKNAME_MIN_LENGTH = 3;
+    public const NICKNAME_MAX_LENGTH = 32;
+    public const NICKNAME_PATTERN = '~^[a-zA-Zа-яА-ЯёЁ0-9]+$~u';
+
+    public const EMAIL_MIN_LENGTH = 10;
+    public const EMAIL_MAX_LENGTH = 32;
+    public const EMAIL_PATTERN = '~^[\.a-z0-9-]+@[\.a-z0-9-]+$~';
+
+    public const PASSWORD_MIN_LENGTH = 6;
+    public const PASSWORD_MAX_LENGTH = 32;
+    public const PASSWORD_PATTERN = '~^(?=.*[0-9])[a-zA-Z0-9]+$~';
+
+    public const SALT_LENGTH = 8;
+    public const SALT_ALPHABET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-#*';
+
+
+    public static function validateNickname(string $nickname): array {
+        $errors = [];
+        if (mb_strlen($nickname) < self::NICKNAME_MIN_LENGTH || mb_strlen($nickname > self::NICKNAME_MAX_LENGTH)) {
+            $errors['nickname'] = 'Invalid length. Allowed length '. self::NICKNAME_MIN_LENGTH . '-' . self::NICKNAME_MAX_LENGTH;
+        } elseif (!preg_match(self::NICKNAME_PATTERN, $nickname)) {
+            $errors['nickname'] = 'Invalid nickname. Special characters are not allowed';
+        }
+        return $errors;
+    }
+
+    public static function validateEmail(string $email): array {
+        $errors = [];
+        if (mb_strlen($email) < self::EMAIL_MIN_LENGTH || mb_strlen($email > self::EMAIL_MAX_LENGTH)) {
+            $errors['email'] = 'Invalid length. Allowed length '. self::EMAIL_MIN_LENGTH . '-' . self::EMAIL_MAX_LENGTH;
+        } elseif (!preg_match(self::EMAIL_PATTERN, $email)) {
+            $errors['email'] = 'Invalid email';
+        } else {
+            $result = DB::getConnection()->prepare('SELECT ID FROM ' . TABLE_USER . ' WHERE email = :email');
+            $result->bindParam(':email', $email);
+            if (!$result->execute()) {
+                http_response_code(500);
+                echo(json_encode(['error' => 'Query can not be executed']));
+                die;
+            }
+            if ($result->fetch(PDO::FETCH_ASSOC)) {
+                $errors['email'] = 'Email already exists';
+            }
+        }
+        return $errors;
+    }
+
+    public static function validatePassword(string $password): array {
+        $errors = [];
+        if (mb_strlen($password) < self::PASSWORD_MIN_LENGTH || mb_strlen($password) > self::PASSWORD_MAX_LENGTH) {
+            $errors['password'] = 'Invalid length. Allowed length '. self::PASSWORD_MIN_LENGTH . '-' . self::PASSWORD_MAX_LENGTH;
+        } elseif (!preg_match(self::PASSWORD_PATTERN, $password)) {
+            $errors['password'] = 'Invalid password. Password should have at least 1 letter and 1 digit';
+        }
+        return $errors;
+    }
+
+    private static function validateSalt(string $salt): bool {
+        $result = DB::getConnection()->prepare('SELECT ID FROM users WHERE salt = :salt');
+        $result->bindParam(':salt', $salt);
+        if (!$result->execute()) {
+            http_response_code(500);
+            echo(json_encode(['error' => 'Query can not be executed']));
+            die;
+        }
+        if ($result->fetch(PDO::FETCH_ASSOC)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static function generateSalt(): string {
+        do {
+            $salt = '';
+            for ($i = 0; $i < self::SALT_LENGTH; $i++) {
+                $salt .= self::SALT_ALPHABET[rand(0, mb_strlen(self::SALT_ALPHABET) - 1)];
+            }
+        } while (!self::validateSalt($salt));
+        return $salt;
+    }
+
+
     public function __construct(int $ID, ?string $accessToken, bool $isAdmin, bool $contentCreator,
                                 bool $privacy, string $nickname, string $email, bool $emailPrivacy, string $password,
                                 string $salt, string $registrationDate, int $balance, bool $balancePrivacy, bool $avatar,
