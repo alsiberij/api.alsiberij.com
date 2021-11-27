df10f86d67163859de2d2589fbf24865313c4330
Alexander Sibirtsev <alsiberij@gmail.com>
2021-11-27 19:43:47 +0300

    Now User entity instance could be obtained via access token

--- entities/creators/UserCreator.php
+++ entities/creators/UserCreator.php
@@ -11,6 +11,21 @@ class UserCreator extends EntityCreator {
             $row['comments'], $row['paidOrders'], $row['lastSeenTime'], $row['lastSeenTimePrivacy']);
     }
 
+    public function newInstanceByAccessToken(string $accessToken) {
+        $result = $this->db->prepare('SELECT * FROM ' . TABLE_USER . ' WHERE accessToken = :token');
+        $result->bindParam(':token', $accessToken);
+        if (!$result->execute()) {
+            http_response_code(500);
+            echo(json_encode(['error'=>'Internal DB error']));
+            die;
+        }
+        if ($r = $result->fetch(PDO::FETCH_ASSOC)) {
+            return $this->constructObject($r);
+        } else {
+            return null;
+        }
+    }
+
     public function newInstance(int $ID): ?User {
         $result = $this->db->prepare('SELECT * FROM ' . TABLE_USER . ' WHERE ID = :ID');
         $result->bindParam(':ID', $ID, PDO::PARAM_INT);
